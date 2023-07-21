ae0cf93d3687f125f5126d418b89cef4fb7dfbb5
Aditi-Dhavale <aditidhavale22@gmail.com>
2023-07-21 13:28:27 +0530

    differences between 2 images

---
+++ differences.py
@@ -0,0 +1,111 @@
+""" from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def image_diff(image_path1, image_path2, output_path=None):
+    # Open the images
+    image1 = Image.open(image_path1)
+    image2 = Image.open(image_path2)
+
+    # Convert the images to numpy arrays
+    array1 = np.array(image1)
+    array2 = np.array(image2)
+
+    # Calculate the absolute difference between the images
+    diff = np.abs(array1 - array2)
+
+    if(diff.any()>50):
+        # Create a black and white image highlighting the differences
+        diff_image = Image.fromarray(diff.astype('uint8'))
+        # Display the images using matplotlib
+        plt.figure(figsize=(10, 5))
+        plt.subplot(1, 3, 1)
+        plt.imshow(image1)
+        plt.title('Image 1')
+
+        plt.subplot(1, 3, 2)
+        plt.imshow(image2)
+        plt.title('Image 2')
+
+        plt.subplot(1, 3, 3)
+        plt.imshow(diff_image)
+        plt.title('Differences')
+
+        plt.show()
+
+        # Save the difference image if output_path is provided
+        if output_path:
+            diff_image.save(output_path)
+    else:
+
+
+# Usage example:
+image1_path = "C://aditi/competitions/hackrxGit/bajajss1.jfif"
+image2_path = "C://aditi/competitions/hackrxGit/bajajss2.jfif"
+output_diff_path = 'C://aditi/competitions/hackrxGit/diff1.jfif'
+
+image_diff(image1_path, image2_path, output_diff_path)
+ """
+
+import cv2 as cv
+import numpy as np
+from PIL import Image, ImageChops
+
+#Ideal Image and The main Image
+img2= cv.imread("C://aditi/competitions/hackrxGit/bajajss1.jfif")
+img1 = cv.imread("C://aditi/competitions/hackrxGit/bajajss2.jfif")
+
+
+#Verifys if there is or isnt a differance in the Image for the If statement
+diff = cv.subtract(img2, img1)
+diff1=np.array(diff)
+results = not np.any(diff1)
+
+#Tells the User if there is a Differance within the 2 images with the model image and the image given
+if results is True:
+    print("The Images are the same!")
+
+else:
+    print("The images are differant")
+
+
+#This is to make the image show the differance to circle
+img_1=Image.open(img1)
+img_2=Image.open(img2)
+diff1=ImageChops.difference(img_1,img_2)
+diff1.save("Differance.jpg")
+
+#Reads the image Just saved
+Differance = cv.imread("Differance.jpg", 0)
+
+#Resize the Image to make it smaller
+
+img1s = cv.resize(img1, (0, 0), fx=0.5, fy=0.5)
+Differance = cv.resize(Differance, (0, 0), fx=0.5, fy=0.5)    
+
+# Find anything not black, i.e. The differance
+nz = cv.findNonZero(Differance)
+
+# Find top, bottom, left and right edge of the Differance
+a = nz[:,0,0].min()
+b = nz[:,0,0].max()
+c = nz[:,0,1].min()
+d = nz[:,0,1].max()
+
+# Average top and bottom edges, left and right edges, to give centre
+c0 = (a+b)/2
+c1 = (c+d)/2
+
+#The Center Coords
+c3 = (int(c0),int(c1))
+
+#Values for the below code so it doesnt look messy
+radius = 50
+color = (0, 0, 255)
+thickness = 2
+
+#This Places a Circle around the center of the differance
+Finished = cv.circle(img1s, c3, radius, color, thickness)
+
+#Saves the Final Image with the circle around it
+cv.imwrite("Final.jpg", Finished)
---
+++ test.py
@@ -0,0 +1,144 @@
+""" import cv2 as cv
+import numpy as np
+from PIL import Image, ImageChops
+import matplotlib.pyplot as plt
+
+# Set the path to the images
+img1_path = "C://aditi/competitions/hackrxGit/bajajss1.jfif"
+img2_path = "C://aditi/competitions/hackrxGit/bajajss2.jfif"
+
+# Load the images
+img1 = cv.imread(img1_path)
+img2 = cv.imread(img2_path)
+
+# Calculate the difference between the two images
+diff = cv.subtract(img2, img1)
+
+# Check if there are any non-zero values in the difference image
+results = not np.any(diff)
+
+# Print a message indicating whether the images are the same or different
+if results:
+    print("The Images are the same!")
+else:
+    print("The images are different")
+
+# Load the images using PIL
+img_1 = Image.open(img1_path)
+img_2 = Image.open(img2_path)
+
+# Calculate the difference between the two images using PIL
+diff = ImageChops.difference(img_1, img_2)
+
+# Save the difference image
+diff.save("Difference.jpg")
+
+# Load the difference image using OpenCV
+Difference = cv.imread("Difference.jpg", 0)
+
+# Resize the images to make them smaller
+img1s = cv.resize(img1, (0, 0), fx=0.5, fy=0.5)
+Difference = cv.resize(Difference, (0, 0), fx=0.5, fy=0.5)
+
+# Find anything not black in the difference image
+nz = cv.findNonZero(Difference)
+
+if nz is not None:
+    # Find top, bottom, left and right edge of the difference
+    a = nz[:, 0, 0].min()
+    b = nz[:, 0, 0].max()
+    c = nz[:, 0, 1].min()
+    d = nz[:, 0, 1].max()
+
+    # Average top and bottom edges, left and right edges, to give center
+    c0 = (a + b) / 2
+    c1 = (c + d) / 2
+
+    # The center coordinates
+    c3 = (int(c0), int(c1))
+
+    # Values for drawing a circle around the center of the difference
+    radius = 50
+    color = (0, 0, 255)
+    thickness = 2
+
+    # Draw a circle around the center of the difference
+    Finished = cv.circle(img1s, c3, radius, color, thickness)
+
+    # Save the final image with the circle around it
+    cv.imwrite("Final.jpg", Finished)
+else:
+    print("No differences found") """
+
+
+import cv2 as cv
+import numpy as np
+
+# load images
+img1 = cv.imread("C://aditi/competitions/hackrxGit/bajajss1.jfif")
+img2 = cv.imread("C://aditi/competitions/hackrxGit/bajajss2.jfif")
+
+# calculate difference
+diff = cv.subtract(img1, img2)  # other order `(img2, img1)` gives worse result
+
+# saves difference
+cv.imwrite("difference.png", diff)
+
+# show difference - press any key to close
+cv.imshow('diff', diff)
+cv.waitKey(0)
+cv.destroyWindow('diff')
+
+if not np.any(diff):
+    print("The images are the same!")
+else:
+    print("The images are differant")
+
+# resize images to make them smaller
+img1_resized = cv.resize(img1, (224, 224), interpolation=cv2.INTER_NEAREST)
+diff_resized = cv.resize(diff, (224, 224), interpolation=cv2.INTER_NEAREST)    #(img1, (0, 0), fx=0.5, fy=0.5)
+#img1_resized = img1
+#diff_resized = diff
+
+# convert to grayscale (without saving and loading again)
+diff_resized = cv.cvtColor(diff_resized, cv.COLOR_BGR2GRAY)
+
+# find anything not black in differance
+non_zero = cv.findNonZero(diff_resized)
+#print(non_zero)
+
+# find top, bottom, left and right edge of the differance
+x_min = non_zero[:,0,0].min()
+x_max = non_zero[:,0,0].max()
+y_min = non_zero[:,0,1].min()
+y_max = non_zero[:,0,1].max()
+print('x:', x_min, x_max)
+print('y:', y_min, y_max)
+
+sizes = [x_max-x_min+1, y_max-y_min+1]
+print('width :', sizes[0])
+print('height:', sizes[1])
+
+# center 
+center_x = (x_min + x_max) // 2
+center_y = (y_min + y_max) // 2
+center = (center_x, center_y)
+print('center:', center)
+
+# radius 
+radius = max(sizes) // 2
+print('radius:', radius)
+
+color = (0, 0, 255)
+thickness = 2
+
+# draw circle around the center of the differance
+finished = cv.circle(img1_resized, center, radius, color, thickness)
+
+# saves final image with circle
+cv.imwrite("final.png", finished)
+
+# show final image - press any key to close
+cv.imshow('finished', finished)
+cv.waitKey(0)
+cv.destroyWindow('finished')
