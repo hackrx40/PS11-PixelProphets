99546be190492ac75f6a8ff98e03a86fc495b7ca
Aditi-Dhavale <114601220+Aditi-Dhavale@users.noreply.github.com>
2023-07-21 21:58:21 +0530

    Add files via upload

---
+++ bbstreamlit2.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+from PIL import Image
+import streamlit as st
+from skimage.metrics import structural_similarity
+import tempfile 
+import os
+
+def boundingBox(path1, path2):
+    # Load images
+    before = cv2.imread(path1)
+    after = cv2.imread(path2)
+
+    # Convert images to grayscale
+    before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+    after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+
+    # Compute SSIM between the two images
+    (score, diff) = structural_similarity(before_gray, after_gray, full=True)
+    print("Image Similarity: {:.4f}%".format(score * 100))
+
+    # The diff image contains the actual image differences between the two images
+    # and is represented as a floating point data type in the range [0,1] 
+    # so we must convert the array to 8-bit unsigned integers in the range
+    # [0,255] before we can use it with OpenCV
+    diff = (diff * 255).astype("uint8")
+    diff_box = cv2.merge([diff, diff, diff])
+
+    # Threshold the difference image, followed by finding contours to
+    # obtain the regions of the two input images that differ
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    mask = np.zeros(before.shape, dtype='uint8')
+    filled_after = after.copy()
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 40:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(before, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.rectangle(after, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.rectangle(diff_box, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.drawContours(mask, [c], 0, (255, 255, 255), -1)
+            cv2.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
+
+    return diff_box
+
+# Streamlit app
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.text('Upload the Image from the listed category.\n[good design or bad design]')
+
+uploaded_file1 = st.file_uploader("Choose the first photo", type=["jpg", "jpeg", "png"])
+uploaded_file2 = st.file_uploader("Choose the second photo", type=["jpg", "jpeg", "png"])
+
+if uploaded_file1 and uploaded_file2 is not None:
+    image1 = Image.open(uploaded_file1)
+    st.image(image1, caption='First Photo', use_column_width=True)
+    image2 = Image.open(uploaded_file2)
+    st.image(image2, caption='Second photo')
+
+    if st.button('COMPARE'):
+        st.write('Result.....')
+        difference = boundingBox(uploaded_file1, uploaded_file2)
+        result_image = Image.fromarray(difference)
+        # Create a temporary file to save the PIL image
+        with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_file:
+            result_image.save(temp_file.name)
+        st.image(temp_file.name, caption='Result photo')
+
+        # Remove the temporary file after displaying the image
+        temp_file.close()
+        os.unlink(temp_file.name)
+
+st.text("")
+st.text('Made by PixelProphets')
