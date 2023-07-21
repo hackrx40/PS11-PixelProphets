9e991a13a14b865f5632c5cbe2315b13ff218f4c
Pixelprophets <140138897+Pixelprophets@users.noreply.github.com>
2023-07-21 16:27:13 +0530

    Add files via upload

---
+++ onlinelearning.py
@@ -0,0 +1,15 @@
+# Online Learning
+
+# Normalize images between 0 and 1 (if not done previously)
+x_train = x_train.astype('float32') / 255.0
+x_val = x_val.astype('float32') / 255.0
+
+# Define the input shape and latent dimension
+input_shape = x_train[0].shape
+latent_dim = 64  # You can adjust this value based on your requirements
+
+# Build the Convolutional Autoencoder model (same as the code provided previously)
+# ...
+
+# Online Learning
+# ...
